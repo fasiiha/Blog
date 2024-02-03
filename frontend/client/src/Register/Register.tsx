2012b91f866f435da8d67754aebc77ded2cdae75
Fasiha Arshad <fasihaa.arshad@gmail.com>
2024-02-04 01:53:07 +0500

    blog

--- frontend/client/src/Register/Register.tsx
+++ frontend/client/src/Register/Register.tsx
@@ -2,7 +2,29 @@ import "./Register.css";
 function Register() {
   return (
     <>
-  
+      <div className="background">
+        <div className="shape"></div>
+        <div className="shape"></div>
+      </div>
+      <form>
+        <h3>Login Here</h3>
+
+        <label for="username">Username</label>
+        <input type="text" placeholder="Email or Phone" id="username" />
+
+        <label for="password">Password</label>
+        <input type="password" placeholder="Password" id="password" />
+
+        <button>Log In</button>
+        <div className="social">
+          <div className="go">
+            <i className="fab fa-google"></i> Google
+          </div>
+          <div className="fb">
+            <i className="fab fa-facebook"></i> Facebook
+          </div>
+        </div>
+      </form>
     </>
   );
 }
