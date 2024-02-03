ac92f48bfa232b3b3cf2f5f1ee8d2fe1b2d7a2fb
Fasiha Arshad <fasihaa.arshad@gmail.com>
2024-02-04 02:47:08 +0500

    blog

--- frontend/client/src/Register/Register.tsx
+++ frontend/client/src/Register/Register.tsx
@@ -1,19 +1,44 @@
+import { useState } from "react";
 import "./Register.css";
 function Register() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function register(ev: { preventDefault: () => void; }){
+    
+    ev.preventDefault();
+    await fetch('http://localhost:8081/signup', {
+      method: 'POST',
+      body: JSON.stringify({username, password}),
+      headers: {'Content-Type': 'application/json'}
+    })
+  }
+
   return (
     <>
       <div className="background">
         <div className="shape"></div>
         <div className="shape"></div>
       </div>
-      <form>
+      <form onSubmit={register}>
         <h3>Login Here</h3>
 
         <label for="username">Username</label>
-        <input type="text" placeholder="Email or Phone" id="username" />
+        <input
+          type="text"
+          placeholder="Email or Phone"
+          value={username}
+          onChange={(ev) => setUsername(ev.target.value)}
+        />
 
         <label for="password">Password</label>
-        <input type="password" placeholder="Password" id="password" />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          value={password}
+          onChange={(ev) => setPassword(ev.target.value)}
+        />
 
         <button>Log In</button>
         <div className="social">
